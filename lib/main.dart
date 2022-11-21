b4c5a2597703d04e8bc611f1461967b164a25146
Serban <serbancalescu@yahoo.com>
2022-11-21 20:06:31 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Curency Converter', ),
-
+      home: const MyHomePage(
+        title: 'Curency Converter',
+      ),
     );
   }
 }
@@ -34,80 +34,67 @@ class _MyHomePageState extends State<MyHomePage> {
   String text1 = "";
   final myController = TextEditingController();
 
-  RegExp digitValidator  = RegExp("[0-9]+");
-bool nr=true;
+  RegExp digitValidator = RegExp("[0-9]+");
+  bool nr = true;
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-
         title: Text(widget.title),
-          centerTitle: true,
+        centerTitle: true,
       ),
-      body:
-
-        Column(
-
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: Image.asset('assets/images/1.jpg').image
-                ),
-              ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(fit: BoxFit.fill, image: Image.asset('assets/images/1.jpg').image),
             ),
-            TextField(
-              keyboardType: TextInputType.number,
-              controller: myController,
-              onChanged: (text) {
-                if(double.parse(text) != null || digitValidator.hasMatch(text)) {
-                  setValidator(true);
-                } else {
-                  setValidator(false);
-                }
-
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter the amount in Euro',
-                errorText: nr ? null : 'Please enter a number',
-              ),
-
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: myController,
+            onChanged: (text) {
+              if (double.parse(text) != null || digitValidator.hasMatch(text)) {
+                setValidator(true);
+              } else {
+                setValidator(false);
+              }
+            },
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter the amount in Euro',
+              errorText: nr ? null : 'Please enter a number',
             ),
-            SizedBox(height: 10,),
-            ElevatedButton(onPressed: (){
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
                 setState(() {
-                  if (double.parse(myController.text) !=null) {
-                    text1 = '${(double.parse(myController.text)*4.85).toStringAsFixed(2)} RON';
+                  if (double.parse(myController.text) != null) {
+                    text1 = '${(double.parse(myController.text) * 4.85).toStringAsFixed(2)} RON';
                   } else {
                     text1 = '';
                   }
                 });
               },
               child: Text('CONVERT!')),
-
-
-
-
-              Text(
-                text1,
-                style: Theme.of(context).textTheme.headline4,
-              ),
-
-
-          ],
-        ),
+          Text(
+            text1,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-  void setValidator(valid){
+
+  void setValidator(valid) {
     setState(() {
-   //   nr = valid;
+      //   nr = valid;
     });
   }
 }
